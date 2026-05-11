ecd59a005865b3601db4701ceb6b80a150b081ed
Faran <faranpannu02@gmail.com>
2026-05-11 18:04:51 +0500

    semester 4 project code

--- dash-boards/assignedsub/manage_sections.php
+++ dash-boards/assignedsub/manage_sections.php
@@ -56,6 +56,7 @@ if (isset($_GET['edit'])) {
 <head>
     <meta charset="UTF-8">
     <title>Section Management | Pars Campus</title>
+    <link rel="icon" type="image/png" href="../../images/pars.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
